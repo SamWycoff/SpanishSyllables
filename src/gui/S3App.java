da9061743e10a42904bbdfe38c9b4e533929279d
Unknown <samantha.wycoff1@yahoo.com>
2018-04-20 15:44:13 -0500

    Fixing Loading Error

--- src/gui/S3App.java
+++ src/gui/S3App.java
@@ -8,12 +8,12 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
 public class S3App extends Application {
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Project 1 GUI.fxml"));
+			BorderPane root = FXMLLoader.load(getClass().getResource("Project 1 GUI.fxml"));
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -23,7 +23,8 @@ public class S3App extends Application {
 		}
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		launch(args);
 	}
 }
+
