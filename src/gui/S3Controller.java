a0f9328ac791a73c3df9c1b0e1c560db3b9d3e95
Unknown <samantha.wycoff1@yahoo.com>
2018-04-28 20:35:41 -0500

    Moved Code from Controller
    This commit is specifically meant to deal with the issues with Design/Readability.

--- src/gui/S3Controller.java
+++ src/gui/S3Controller.java
@@ -4,12 +4,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.Scanner;
-
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import separator.*;
 
 public class S3Controller {
@@ -34,78 +31,8 @@ public class S3Controller {
 	
 	@FXML
 	public void separate() {
-		String text = original.getText();
-		if (text.length() > 1000) {
-			Alert alert = new Alert(AlertType.ERROR);
-			alert.setTitle("Error");
-			alert.setHeaderText("This text is over 1000 characters and, therefore, too long to separate.");
-			alert.setContentText("The current provided text contains " + text.length() + " characters.");
-			alert.show();
-		}
-		else {
-			separateHelper(text);
-		}
-	}
-	
-	public void separateHelper(String text) {
-		char[] characters = text.toCharArray();
-		for (int i = 0; i < characters.length; i++) {
-			addSlashes(characters, i);
-		}
-		text = String.valueOf(characters);
-		String[] tempWords = text.split(" ");
-		String finalResult = "";
-		for (int i = 0; i < tempWords.length; i++) {
-			finalResult = resyllabification(finalResult, new Word(tempWords[i]).getSeparated());
-		}
-		result.setText(String.format(finalResult));
-	}
-	
-	public void addSlashes(char[] characters, int i) {
-		if (!Letter.isLetter(characters[i]) && characters[i] != ' ') {
-			characters[i] = '/';
-			avoidTwoSlashes(characters, i);
-		}
-	}
-	
-	public void avoidTwoSlashes(char[] characters, int i) {
-		if (i + 1 != characters.length && !Letter.isLetter(characters[i + 1]) 
-				&& characters[i + 1] != ' ' && !Letter.isLetter(characters[i])) {
-			characters[i] = Character.MIN_VALUE;
-		}
-	}
-	
-	public String resyllabification(String previous, String present) {
-		if (previous.length() == 0) {
-			return present;
-		}
-		else if (present.length() == 0) {
-			return previous;
-		}
-		else if (previous.charAt(previous.length() - 1) == '/' || Letter.accent(present.charAt(0)) 
-				|| (!Letter.isLetter(present.charAt(0)) && Letter.accent(present.charAt(1)))) {
-			return previous + present;
-		}
-		else if (Letter.consonant(previous.charAt(previous.length() - 2)) && (Letter.vowel(present.charAt(0)) 
-				|| (!Letter.isLetter(present.charAt(0)) && Letter.vowel(present.charAt(1))))) {
-			return resyllabificationHelper(previous, present);
-		}
-		else {
-			return previous + present;
-		}
-	}
-	
-	public String resyllabificationHelper(String previous, String present) {
-		if (!Letter.isLetter(present.charAt(0))) {
-			return previous.substring(0, previous.length() - 2) + ".'" 
-			+ previous.substring(previous.length() - 2, previous.length() - 1) 
-			+ present.substring(1);
-		}
-		else {
-			return previous.substring(0, previous.length() - 2) + "." 
-					+ previous.substring(previous.length() - 2, previous.length() - 1) 
-					+ present;
-		}
+		String separated = TextSeparator.separate(original.getText());
+		result.setText(separated);
 	}
 	
 	//Copied from example during Wednesday's lecture
