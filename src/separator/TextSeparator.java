f1b5db52f081dc7ede874a125e6a5b07e781684e
Unknown <samantha.wycoff1@yahoo.com>
2018-05-04 03:05:45 -0500

    TextSeparatorTest

--- src/separator/TextSeparator.java
+++ src/separator/TextSeparator.java
@@ -4,6 +4,8 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 
 public class TextSeparator {
+	static char[] characters;
+	
 	public static String separate(String text) {
 		if (text.length() > 1000) {
 			Alert alert = new Alert(AlertType.ERROR);
@@ -19,9 +21,9 @@ public class TextSeparator {
 	}
 	
 	private static String separateHelper(String text) {
-		char[] characters = text.toCharArray();
-		for (int i = 0; i < characters.length; i++) {
-			addSlashes(characters, i);
+		characters = text.toCharArray();
+		for (int i = characters.length - 1; i >= 0; i--) {
+			addSlashes(i);
 		}
 		text = String.valueOf(characters);
 		String[] tempWords = text.split(" ");
@@ -32,26 +34,36 @@ public class TextSeparator {
 		return finalResult;
 	}
 	
-	private static void addSlashes(char[] characters, int i) {
+	private static void addSlashes(int i) {
 		if (!Letter.isLetter(characters[i]) && characters[i] != ' ') {
 			characters[i] = '/';
-			avoidTwoSlashes(characters, i);
+			avoidTwoSlashes(i);
 		}
 	}
 	
-	private static void avoidTwoSlashes(char[] characters, int i) {
-		if (i + 1 != characters.length && !Letter.isLetter(characters[i + 1]) 
-				&& characters[i + 1] != ' ' && !Letter.isLetter(characters[i])) {
-			characters[i] = Character.MIN_VALUE;
+	private static void avoidTwoSlashes(int i) {
+		if (i - 1 != 0 && !Letter.isLetter(characters[i - 1]) 
+				&& characters[i - 1] != ' ' && !Letter.isLetter(characters[i])) {
+			String tempString = String.valueOf(characters);
+			tempString = tempString.substring(0, i) + tempString.substring(i + 1);
+			characters = tempString.toCharArray();
+		}
+		if (i - 1 != 0 && i + 1 != characters.length && Letter.isLetter(characters[i - 1]) && Letter.isLetter(characters[i + 1])) {
+			String tempString = String.valueOf(characters);
+			tempString = tempString.substring(0, i) + " "+ tempString.substring(i);
+			characters = tempString.toCharArray();
 		}
 	}
 	
-	private static String resyllabification(String previous, String present) {
+	protected static String resyllabification(String previous, String present) {
 		if (previous.length() == 0) {
 			return present;
 		}
 		else if (present.length() == 0) {
 			return previous;
+		}
+		else if (previous.charAt(previous.length() - 1) == '.' && present.charAt(0) == '/') {
+			return previous.substring(0, previous.length() - 1) + present;
 		}
 		else if (previous.charAt(previous.length() - 1) == '/' || Letter.accent(present.charAt(0)) 
 				|| (!Letter.isLetter(present.charAt(0)) && Letter.accent(present.charAt(1)))) {
--- src/separator/TextSeparatorTest.java
+++ src/separator/TextSeparatorTest.java
@@ -7,8 +7,34 @@ import org.junit.Test;
 public class TextSeparatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSeparator() {
+		String noSlashes = TextSeparator.separate("El león es un animal magnífico");
+		assertTrue(noSlashes.equals("'El.le.'ó.'ne.'su.na.ni.'mal.mag.'ní.fi.co."));
+		String oneSlash = TextSeparator.separate("El león es un animal magnífico.");
+		assertTrue(oneSlash.equals("'El.le.'ó.'ne.'su.na.ni.'mal.mag.'ní.fi.co/"));
+		String multipleSlashes = TextSeparator.separate("El león...es un animal magnífico.");
+		assertTrue(multipleSlashes.equals("'El.le.'ón/'e.'su.na.ni.'mal.mag.'ní.fi.co/"));
 	}
-
+	
+	@Test
+	public void testResyllabification() {
+		String noPrevious = TextSeparator.resyllabification("", "co.mer.");
+		String noPresent = TextSeparator.resyllabification("co.mer.", "");
+		String periodSlash = TextSeparator.resyllabification("co.mer.", "/cho.co.la.te.");
+		String slash = TextSeparator.resyllabification("co.mer/", "cho.co.la.te.");
+		String accent = TextSeparator.resyllabification("co.mer.", "ár.bo.les.");
+		String accentWithSlash = TextSeparator.resyllabification("co.mer.", "/ár.bo.les.");
+		String consonantVowel = TextSeparator.resyllabification("co.mer.", "a.or.ta.");
+		String consonantVowelStress = TextSeparator.resyllabification("co.mer.", "'es.");
+		String other = TextSeparator.resyllabification("a.or.ta.", "co.mer.");
+		assertTrue(noPrevious.equals("co.mer."));
+		assertTrue(noPresent.equals("co.mer."));
+		assertTrue(periodSlash.equals("co.mer/cho.co.la.te."));
+		assertTrue(slash.equals("co.mer/cho.co.la.te."));
+		assertTrue(accent.equals("co.mer.ár.bo.les."));
+		assertTrue(accentWithSlash.equals("co.mer/ár.bo.les."));
+		assertTrue(consonantVowel.equals("co.me.ra.or.ta."));
+		assertTrue(consonantVowelStress.equals("co.me.'res."));
+		assertTrue(other.equals("a.or.ta.co.mer."));
+	}
 }
